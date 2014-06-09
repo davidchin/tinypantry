874e0f4d41d6e8f43d6679f9f4609e2d80eaac63
davidchin <davidchin.yf@gmail.com>
2014-06-09 23:18:01 +1000

    Rudimentry recipes import

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,2 +1,24 @@
+require 'open-uri'
+
 class Feed < ActiveRecord::Base
+  # has_many :recipes
+
+  def parse_rss
+    xml_file = open(rss)
+    xml = Nokogiri::XML(xml_file)
+  end
+
+  def import_rss
+    xml = parse_rss
+
+    xml.xpath('//item').map do |item|
+      data = {
+        name: item.at_xpath('.//title').content,
+        url: item.at_xpath('.//link').content,
+        description: item.at_xpath('.//description').content
+      }
+
+      Recipe.create(data)
+    end
+  end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ActiveRecord::Base
   has_attached_file :image, styles: { thumb: '100x100>' }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+  # belongs_to :feed
 end
