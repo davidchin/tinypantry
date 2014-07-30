e6ad97070604a819809591f832f415a2b673fd89
davidchin <davidchin.yf@gmail.com>
2014-07-30 22:12:42 +1000

    Multi-line block

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -4,15 +4,15 @@ class ErrorsController < ActionController::Base
     @status_code = ActionDispatch::ExceptionWrapper.new(env, @exception).status_code
 
     respond_to do |format|
-      format.html {
+      format.html do
         render file: "public/#{ @status_code }.html",
                status: @status_code
-      }
+      end
 
-      format.json {
+      format.json do
         render json: { error: @exception.message, status: @status_code },
                status: @status_code
-      }
+      end
     end
   end
 end
