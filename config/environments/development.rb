84728c8c40e7c7fce508e6db42d6580a60be6ae4
davidchin <davidchin.yf@gmail.com>
2014-10-26 00:46:38 +1100

    Heroku sendgrid settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,7 @@ Rails.application.configure do
   # Mailer config
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
   # Paperclip config
