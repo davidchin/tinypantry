270e4b1dc2703f5cc3fc5121a1a19ba4ea9378e2
davidchin <davidchin.yf@gmail.com>
2014-10-31 21:17:42 +1100

    Extra checks for bots

--- lib/snapshot/renderer.rb
+++ lib/snapshot/renderer.rb
@@ -1,11 +1,29 @@
 module Snapshot
   class Renderer
+    BOTS = [
+      'baiduspider',
+      'facebookexternalhit',
+      'twitterbot',
+      'rogerbot',
+      'linkedinbot',
+      'embedly',
+      'bufferbot',
+      'quora link preview',
+      'showyoubot',
+      'outbrain',
+      'pinterest',
+      'developers.google.com/+/web/snippet',
+      'slackbot'
+    ]
+
     def initialize(app)
       @app = app
     end
 
     def call(env)
-      if fragment = parse_fragment(env)
+      fragment = parse_fragment(env)
+
+      if fragment || (bot_request?(env) && page_request?(env))
         render_fragment(env, fragment)
       else
         @app.call(env)
@@ -24,11 +42,29 @@ module Snapshot
 
     def render_fragment(env, fragment)
       url = "#{ env['rack.url_scheme'] }://#{ env['HTTP_HOST'] }#{ fragment[:path] }"
-      url = url + "?#{ fragment[:query] }" if fragment[:query].present?
+      url += "?#{ fragment[:query] }" if fragment[:query].present?
 
       output = `node_modules/.bin/phantomjs lib/snapshot/browser.js #{ url } --load-images=false`
 
       Rack::Response.new(output, 200, [])
+    end
+
+    def bot_request?(env)
+      user_agent = env['HTTP_USER_AGENT']
+      buffer_agent = env['X-BUFFERBOT']
+
+      buffer_agent || BOTS.any? { |bot| bot.include?(user_agent.downcase) }
+    end
+
+    def page_request?(env)
+      method = env['REQUEST_METHOD']
+      accept = env['HTTP_ACCEPT']
+      path = env['REQUEST_PATH']
+
+      method.upcase == 'GET' &&
+      accept =~ /text\/html/ &&
+      !(path =~ /^\/(?:assets|api)/) &&
+      !(path =~ /\.[a-z0-9]{2,4}$/i)
     end
   end
 end
