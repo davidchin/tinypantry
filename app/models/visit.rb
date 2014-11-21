4751732475b458a17340943007c107bc7767a054
davidchin <davidchin.yf@gmail.com>
2014-11-22 07:32:35 +1100

    Provide some feedback

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -11,16 +11,20 @@ class Visit < ActiveRecord::Base
   def self.import_total_count(page = 1)
     starts_at = Date.new(2014, 1, 1)
     data = fetch_recipe_views(starts_at, page)
+    visits = []
 
     data[:formated_rows].each do |row|
       next unless row[:visitable_id]
       visit = find_or_initialize_by(visitable_id: row[:visitable_id], visitable_type: 'Recipe')
       visit.total_count = row[:count]
       visit.save
+      visits << visit
     end
 
     num_page = data[:num_pages] || 0
     import_total_count(page + 1) if num_page > page
+
+    return visits
   end
 
   def self.import_last_30_days_count(page = 1)
@@ -32,10 +36,13 @@ class Visit < ActiveRecord::Base
       visit = find_or_initialize_by(visitable_id: row[:visitable_id], visitable_type: 'Recipe')
       visit.last_30_days_count = row[:count]
       visit.save
+      visits << visit
     end
 
     num_page = data[:num_pages] || 0
     import_last_30_days_count(page + 1) if num_page > page
+
+    return visits
   end
 
   def self.fetch_recipe_views(start_date, page = 1)
