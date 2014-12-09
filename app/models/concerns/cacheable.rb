a2501e4ff633ee7ff64af4cbb430aa633fb7f7e8
davidchin <davidchin.yf@gmail.com>
2014-12-10 06:49:25 +1100

    Fix typoe

--- app/models/concerns/cacheable.rb
+++ app/models/concerns/cacheable.rb
@@ -25,7 +25,7 @@ module Cacheable
 
     def self.last_updated_at
       # where(nil).pluck(:updated_at).max
-      where(nil).order_by(updated_at: :desc).first.try(:updated_at)
+      where(nil).order(updated_at: :desc).first.try(:updated_at)
     end
   end
 end
